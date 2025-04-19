33a6a09a4d0a86b58b3a500633fcd756d35b06cc
Manthan Solanki <manthansolanki1999@gmail.com>
2025-04-19 16:34:23 +0200

    Revert back the changes of cloudflare

--- next.config.js
+++ next.config.js
@@ -1,5 +1,4 @@
 const { withContentlayer } = require('next-contentlayer2')
-const { setupDevPlatform } = require('@cloudflare/next-on-pages/next-dev')
 
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
@@ -62,12 +61,7 @@ const unoptimized = process.env.UNOPTIMIZED ? true : undefined
 /**
  * @type {import('next/dist/next-server/server/config').NextConfig}
  **/
-module.exports = async () => {
-  // Setup Cloudflare Pages development platform
-  if (process.env.NODE_ENV === 'development') {
-    await setupDevPlatform()
-  }
-
+module.exports = () => {
   const plugins = [withContentlayer, withBundleAnalyzer]
   return plugins.reduce((acc, next) => next(acc), {
     output,
